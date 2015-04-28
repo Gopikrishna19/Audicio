80052beb0c1f6ea10cdc9e086c485f3c49a89320
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2015-04-28 12:37:13 -0400

    Website logo consistency

--- Views/profile/config.php
+++ Views/profile/config.php
@@ -4,8 +4,8 @@
             <button class="skip" ng-click="next()">Skip</button>
             <button class="next" ng-click="next()">{{btnNext}}</button>
         </div>
+        <div class="logo">Audicio</div>
         <ul>
-            <li class="logo">Audicio</li>
             <li class="title">Configuration</li>
             <li ng-repeat="step in steps" step="{{step}}" ng-class="{on:onPage(step)}"></li>
             <li step="Finish"><span class="sep"></span></li>
