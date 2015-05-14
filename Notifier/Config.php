c02bba55dc6cfc0e42c599c4a449d652a1e21c6c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2015-05-14 09:40:42 -0400

    Project htaccess fix

---
+++ Notifier/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DB_HOST", "mydbinstance.cj6mavfxam3w.us-east-1.rds.amazonaws.com");
+define("DB_NAME", "audicio");
+define("DB_USER", "admin");
+define("DB_PASS", "adminadmin");
+
+define("AWS_KEY", "AKIAJOMY5V6OQFE5HERQ");
+define("AWS_SECRET", "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");
+
+define("Audicio", "http://audicio.elasticbeanstalk.com");
